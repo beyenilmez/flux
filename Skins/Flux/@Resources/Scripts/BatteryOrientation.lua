b6b355b10fe32fe3ef7efe338435c663f68cfe5e
Bedirhan Yenilmez <76536654+beyenilmez@users.noreply.github.com>
2023-06-10 01:03:00 +0300

    Added background and battery texts
    -Dynamic background
    -Battery percentage and remaining battery time texts

--- Skins/Flux/@Resources/Scripts/BatteryOrientation.lua
+++ Skins/Flux/@Resources/Scripts/BatteryOrientation.lua
@@ -1,21 +1,27 @@
-PROPERTIES = {}
-
 function Initialize()
     PercentageDirection = tonumber(SKIN:GetVariable('Orientation', '0'))
-    PoleHeight = SKIN:GetVariable('PoleHeight', '30')
-    PoleWidth = SKIN:GetVariable('PoleWidth', '85')
-    PoleRadius = SKIN:GetVariable('PoleRadius', '14')
 
     if(PercentageDirection > 0) then
+        --  Switch shell width/height
         ShellWidth = SKIN:GetVariable('ShellWidth', '290')
         ShellHeight = SKIN:GetVariable('ShellHeight', '550')
-
         SKIN:Bang('!SetVariable ShellWidth '..ShellHeight)
         SKIN:Bang('!SetVariable ShellHeight '..ShellWidth)
 
-        SKIN:Bang('!SetVariable OffsetY '..PoleHeight)
-        SKIN:Bang('!SetVariable PoleHeight '..(PoleHeight + PoleRadius))
+        SKIN:Bang('!SetVariable PoleHeight "(#PoleHeight# + #PoleRadius#)"')
+
+        SKIN:Bang('!SetVariable BackgroundHeight "(#BackgroundPaddingTop# + #ShellHeight# + #BackgroundPaddingBottom# + #PoleHeight# - #PoleRadius#)"')
+        SKIN:Bang('!SetVariable BackgroundWidth "(#BackgroundPaddingLeft# + #ShellWidth# + #BackgroundPaddingRight#)')
+        SKIN:Bang('!SetVariable OffsetX "#BackgroundPaddingLeft#"')
+        SKIN:Bang('!SetVariable OffsetY "(#BackgroundPaddingTop# + #PoleHeight# - #PoleRadius#)"')
+
+        SKIN:Bang('!SetVariable VerticalTextOffset "(#PoleHeight# - #PoleRadius#)"')
     else
-        SKIN:Bang('!SetVariable PoleHeight '..(PoleHeight + PoleRadius))
+        SKIN:Bang('!SetVariable PoleHeight "(#PoleHeight# + #PoleRadius#)"')
+
+        SKIN:Bang('!SetVariable BackgroundHeight "(#BackgroundPaddingTop# + #ShellHeight# + #BackgroundPaddingBottom#)"')
+        SKIN:Bang('!SetVariable BackgroundWidth "(#BackgroundPaddingLeft# + #ShellWidth# + #PoleHeight# - #PoleRadius# + #BackgroundPaddingRight#)')
+        SKIN:Bang('!SetVariable OffsetX "#BackgroundPaddingLeft#"')
+        SKIN:Bang('!SetVariable OffsetY "#BackgroundPaddingTop#"')
     end
 end
